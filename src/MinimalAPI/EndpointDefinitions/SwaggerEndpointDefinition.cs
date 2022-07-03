3a077f5cc55595a604a36e0dfdec8765443cc640
leandrochomp <chomp.vercetti@gmail.com>
2022-07-03 17:19:51 +1000

    Adding Swagger support

---
+++ src/MinimalAPI/EndpointDefinitions/SwaggerEndpointDefinition.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+using MinimalAPI.SecrecetSauce;
+
+namespace MinimalAPI.EndpointDefinitions
+{
+    public class SwaggerEndpointDefinition : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinimalAPI V1"));
+        }
+
+        public void DefineServices(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API", Version = "v1" });
+                c.EnableAnnotations();
+            });
+        }
+    }
+}
--- src/MinimalAPI/Program.cs
+++ src/MinimalAPI/Program.cs
@@ -1,12 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
-using MinimalAPI.Models;
-using MinimalAPI.Repositories;
+using MinimalAPI.SecrecetSauce;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointDefinitions();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+app.UseEndpointDefinitions();
 
 app.Run();
